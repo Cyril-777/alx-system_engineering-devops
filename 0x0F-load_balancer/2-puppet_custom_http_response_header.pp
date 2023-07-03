a0606ff5d2e26c7c6b0c05af81a7ef797f29b3d1
Cyril-777 <kyrillosmaher10@gmail.com>
2023-07-03 15:09:43 -0700

    puppet automation advanced task solved

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,8 @@
+# puppet manifest that configures a brand new Ubuntu machine to the requirements
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
+}
