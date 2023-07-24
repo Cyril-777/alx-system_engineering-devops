0ecaa2f33e9e904b97a891221a1ae929ea60a41a
Cyril-777 <kyrillosmaher10@gmail.com>
2023-07-24 12:11:52 -0700

    first task solved

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""script that for a given employee ID, returns information
+about his/her TODO list progress"""
+import requests
+from sys import argv
+
+
+def todolist(user_id):
+    """outputs the employee TODO list progress"""
+    name = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(
+            user_id)).json().get('name')
+    tasks = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+            user_id)).json()
+    completed_tasks = ['\t {}\n'.format(dic.get('title')) for dic in tasks
+                 if dic.get('completed')]
+    if name and tasks:
+        print("Employee {} is done with tasks({}/{}):".format
+              (name, len(completed_tasks), len(tasks)))
+        print(''.join(completed_tasks), end='')
+
+
+if __name__ == '__main__':
+    if len(argv) == 2:
+        todolist(int(argv[1]))
