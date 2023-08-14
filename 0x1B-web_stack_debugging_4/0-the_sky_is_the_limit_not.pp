fed3925b6185d5c77b20e2805b89eb3a4968548a
Cyril-777 <kyrillosmaher10@gmail.com>
2023-08-14 06:58:56 -0700

    task #0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# puppet auto fix for nginx not handling all requests
+
+exec { 'fix-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Restart Nginx
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
